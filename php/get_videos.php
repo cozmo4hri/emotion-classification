49fa98f9a46ae0648e82ca9743f7fe754a2b39fb
Martin Hoffmann <maedhoff@ucsc.edu>
2019-08-13 21:46:17 +0100

    Added a simple algorithm implementation in python to select videos that are shown to user

--- php/get_videos.php
+++ php/get_videos.php
@@ -1,4 +1,20 @@
 <?php
-    $video_ids = [1,2,3];
+    /*
+    Algorithm draft:
+    0) p=0
+    1) select all videos that have been rated p times
+    2) try to find random videos from n different categories => done
+    3) if not possible, add videos to selection that have been rated ++p times
+    4) go back to 2)
+    */
+
+    $n = intval($_GET["n"]);
+    
+    $video_ids = [];
+
+    for($i = 0; $i<$n; $i++){
+        array_push($video_ids, $i+1);
+    }
+
     echo json_encode($video_ids);
 ?>
