6327282c6cac23058f3e407de352ce7cd12be81a
Martin Hoffmann <maedhoff@ucsc.edu>
2019-08-22 18:03:45 +0100

    Added a way to automatically fill out prolific ID

--- index.php
+++ index.php
@@ -1,2 +1,10 @@
-<?php header("Location: /site.html"); ?>
+<?php 
+    $redirect = "Location: /site.html";
+    $uri_params = explode("/",$_SERVER['REQUEST_URI']);
+
+    if( strlen($uri_params[1]) > 0 ) {
+        $redirect .= ("?pid=" . $uri_params[1]);
+    }
+    header($redirect); 
+?>
 Server not running!
